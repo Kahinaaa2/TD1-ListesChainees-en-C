156a8f038b581f649c81422b72b06d8a08fc36b3
HAMMAD Kahina <166177981+Kahinaaa2@users.noreply.github.com>
2024-11-01 12:05:28 +0100

    Create Exercice3.c

---
+++ Exercice3.c
@@ -0,0 +1,7 @@
+ Exercice 3 (Suppression de Chaînon)
+ 1. Définir une liste chaînée d’entiers composée de 10 Chainons dont les valeurs seront aléatoires entre 0 et 5 compris.
+ 2. Écrire des fonctions prenant en paramètre un pointeur sur liste et un entier qui permettent de supprimer dans une
+ liste :
+ — Lepremier Chainon dont la valeur est égale au paramètre.
+ — Tous les Chainon dont la valeur est égale au paramètre.
+ 3. Tester ces fonctions avec la liste de la question 1
